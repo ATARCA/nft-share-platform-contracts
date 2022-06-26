ab5750494134eb367c045fb98b68b772771bcea4
Jarno Marttila <jarno.marttila@sujuwa.fi>
2022-06-26 16:32:29 +0200

    Deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,26 @@
+const tokenURIBase = 'domain/metadata/';
+
+
 async function main() {
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
+  
+  ShareableTokenContract = await ethers.getContractFactory("ShareableERC721");
+  instanceShareableTokenContract = await ShareableTokenContract.deploy("ShareableToken", "Streamr Contribution Token")
+  instanceShareableTokenContract.setBaseURI(tokenURIBase);
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
+  EndorsableTokenContract = await ethers.getContractFactory("EndorseERC721");
+  instanceEndorsableTokenContract = await EndorsableTokenContract.deploy("EndorseERC721", "Streamr Endorsement Token")
 
-  console.log("Token address:", token.address);
+  LikeTokenContract = await ethers.getContractFactory("LikeERC721");
+  instanceLikeTokenContract = await LikeTokenContract.deploy("LikeERC721", "Streamr Like Token")
+
+  console.log("Contribution token address:", instanceShareableTokenContract.address);
+  console.log("Endorsement token address:", instanceEndorsableTokenContract.address);
+  console.log("Like token address:", instanceLikeTokenContract.address);
 }
 
 main()
