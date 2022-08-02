ed9074ab3acc3b880fe9ff08ac8f033ab035fb85
Martin Moravek <martin.m.moravek@gmail.com>
2022-08-02 14:14:49 +0200

    update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,34 +1,79 @@
 const tokenURIBase = 'domain/metadata/';
+const _ = require("lodash");
 
 
 async function main() {
+
+  const findEvent = function(eventName, transactionReceipt) {
+    return _.filter(transactionReceipt?.events, function(o) {
+      return o.event == eventName
+    });
+  }
+
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
 
   console.log("Account balance:", (await deployer.getBalance()).toString());
   
-  ShareableTokenContract = await ethers.getContractFactory("ShareableERC721");
-  instanceShareableTokenContract = await ShareableTokenContract.deploy("ShareableToken", "Streamr Contribution Token")
-  await instanceShareableTokenContract.setBaseURI(tokenURIBase);
+  let ShareableERC721;
+  let _shareableERC721;
+  let LikeERC721;
+  let _likeERC721;
+  let EndorseableERC721;
+  let _endorseableERC721;
 
-  EndorsableTokenContract = await ethers.getContractFactory("EndorseERC721");
-  instanceEndorsableTokenContract = await EndorsableTokenContract.deploy("EndorseERC721", "Streamr Endorsement Token")
-  await instanceEndorsableTokenContract.setProjectAddress(instanceShareableTokenContract.address)
+  let FactoryContract;
+  let _factoryContract;
 
-  LikeTokenContract = await ethers.getContractFactory("LikeERC721");
-  instanceLikeTokenContract = await LikeTokenContract.deploy("LikeERC721", "Streamr Like Token")
-  await instanceLikeTokenContract.setProjectAddress(instanceShareableTokenContract.address)
+  console.log("Deploying ShareableERC721");
+  ShareableERC721 = await ethers.getContractFactory("ShareableERC721");
+  _shareableERC721 = await ShareableERC721.deploy();
+  await _shareableERC721.deployed()
+  console.log("Deploying ShareableERC721-DONE");
 
-  let likeProjectaddress = await instanceLikeTokenContract.getProjectAddress()
-  let endorseProjectaddress = await instanceEndorsableTokenContract.getProjectAddress()
+  console.log("Deploying LikeERC721");
+  LikeERC721 = await ethers.getContractFactory("LikeERC721");
+  _likeERC721 = await LikeERC721.deploy();
+  await _likeERC721.deployed()
+  console.log("Deploying LikeERC721-DONE");
 
-  console.log("Shareable token address: ", instanceShareableTokenContract.address);
-  console.log("Endorsement token address: ", instanceEndorsableTokenContract.address);
-  console.log("Like token address: ", instanceLikeTokenContract.address);
+  console.log("Deploying EndorseERC721");
+  EndorseableERC721 = await ethers.getContractFactory("EndorseERC721");
+  _endorseableERC721 = await EndorseableERC721.deploy();
+  await _endorseableERC721.deployed()
+  console.log("Deploying EndorseERC721-DONE");
 
-  console.log('Endorsement token contracts project address set to:', endorseProjectaddress)
-  console.log('Like token contracts project address set to:', likeProjectaddress)
+  FactoryContract = await ethers.getContractFactory("TalkoFactory");
+  _factoryContract = await FactoryContract.deploy(_shareableERC721.address, _likeERC721.address, _endorseableERC721.address);
+  await _factoryContract.deployed()
+
+  console.log("Factory address: ", _factoryContract.address);
+
+  let shareableTokenDeployedProxyAddress = await _factoryContract.createSProxy("ShareableToken","ST",0, deployer.address);
+  let shareableTokenReceipt = await shareableTokenDeployedProxyAddress.wait()
+  let shareableTokenEvent = findEvent('ShareableERC721ProxyCreated', shareableTokenReceipt)
+  let shareableTokenDeployAddress = shareableTokenEvent[0]?.args[0]
+
+  console.log("Shareable token address: ", shareableTokenDeployAddress);
+
+  let likeTokenDeployedProxyAddress = await _factoryContract.createLProxy("LikeERC721","LT",0, deployer.address);
+  let likeTokenReceipt = await likeTokenDeployedProxyAddress.wait()
+  let likeTokenEvent = findEvent('LikeERC721ProxyCreated', likeTokenReceipt)
+  let likeTokenDeployAddress = likeTokenEvent[0]?.args[0]
+  let proxiedLikeToken = await LikeERC721.attach(likeTokenDeployAddress);
+  await proxiedLikeToken.setProjectAddress(shareableTokenDeployAddress);
+
+  console.log("LikeERC721 token address: ", likeTokenDeployAddress);
+
+  let endorseTokenDeployedProxyAddress = await _factoryContract.createEProxy("EndorseERC721","ET",0, deployer.address);
+  let endorseTokenReceipt = await endorseTokenDeployedProxyAddress.wait()
+  let endorseTokenEvent = findEvent('EndorseERC721ProxyCreated', endorseTokenReceipt)
+  let endorseTokenDeployAddress = endorseTokenEvent[0]?.args[0]
+  let proxiedEndorseToken = await EndorseableERC721.attach(endorseTokenDeployAddress)
+  await proxiedEndorseToken.setProjectAddress(shareableTokenDeployAddress)
+
+  console.log("EndorseERC721 token address: ", likeTokenDeployAddress);
 }
 
 main()
