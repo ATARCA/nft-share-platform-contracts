984f1dab9ab87d8f745df346ed9a38535ef3c5e7
Jarno <jarno.marttila@sujuwa.fi>
2022-08-08 16:00:38 +0200

    Talko factory can have multiple operators to create proxies

--- contracts/TalkoFactory.sol
+++ contracts/TalkoFactory.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./ShareableERC721.sol";
-import "./EndorseERC721.sol";
-import "./LikeERC721.sol";
-
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
-
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./ShareableERC721.sol";
+import "./EndorseERC721.sol";
+import "./LikeERC721.sol";
 import "./TokenBeacon.sol";
 
-contract TalkoFactory { //Todo: access control
+
+contract TalkoFactory is AccessControl { //Todo: access control
+
+  bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
   uint256 internal _indexShareableERC721ProxyInstance;
   uint256 internal _indexLikeERC721ProxyInstance;
@@ -31,10 +33,28 @@ contract TalkoFactory { //Todo: access control
 
   constructor(address _shareableTokenBeacon_vLogic, address _likeTokenBeacon_vLogic, address _endorseTokenBeacon_vLogic) {
     //set all beacons up with deployed instances of contracts
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _setupRole(OPERATOR_ROLE, msg.sender);
     s_beacon = new TokenBeacon(_shareableTokenBeacon_vLogic, msg.sender);
     l_beacon = new TokenBeacon(_likeTokenBeacon_vLogic, msg.sender);
     e_beacon = new TokenBeacon(_endorseTokenBeacon_vLogic, msg.sender);
   }
+
+    function addOperator(address newOperater) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(OPERATOR_ROLE, newOperater);
+    }
+
+    function removeOperator(address operator) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _revokeRole(OPERATOR_ROLE, operator);
+    }
+
+    function addAdmin(address newAdmin) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(DEFAULT_ADMIN_ROLE, newAdmin);
+    }
+
+    function removeAdmin(address admin) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _revokeRole(DEFAULT_ADMIN_ROLE, admin);
+    }
 
   function getIndexForShareableERC721ProxyInstance() public view returns(uint256) {
     return _indexShareableERC721ProxyInstance;
@@ -96,7 +116,7 @@ contract TalkoFactory { //Todo: access control
     return address(e_beacon);
   }
 
-  function createShareableERC721Proxy(string memory _name, string memory _symbol, address _owner) external returns(address) {
+  function createShareableERC721Proxy(string memory _name, string memory _symbol, address _owner) external onlyRole(OPERATOR_ROLE) returns(address) {
     setShareableProxiesName(_name);
 
     BeaconProxy proxy = new BeaconProxy(
@@ -108,7 +128,7 @@ contract TalkoFactory { //Todo: access control
     return address(proxy);
   }
 
-  function createLikeERC721Proxy(string memory _name, string memory _symbol, address _owner) external returns(address) {
+  function createLikeERC721Proxy(string memory _name, string memory _symbol, address _owner) external onlyRole(OPERATOR_ROLE) returns(address) {
     setLikeableProxiesName(_name);
     BeaconProxy proxy = new BeaconProxy(
       address(l_beacon),
@@ -119,7 +139,7 @@ contract TalkoFactory { //Todo: access control
     return address(proxy);
   }
 
-  function createEndorseERC721Proxy(string memory _name, string memory _symbol, address _owner) external returns(address) {
+  function createEndorseERC721Proxy(string memory _name, string memory _symbol, address _owner) external onlyRole(OPERATOR_ROLE) returns(address) {
     setEndorsableProxiesName(_name);
     BeaconProxy proxy = new BeaconProxy(
       address(e_beacon),
