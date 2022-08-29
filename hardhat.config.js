361272f29e2fb0f55b2d0b9c041c97f3b5839e98
Jarno <jarno.marttila@sujuwa.fi>
2022-08-29 12:06:02 +0200

    prefixes to wallet private key, added polygonscan api key

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,9 +10,8 @@ require('solidity-coverage')
 
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY_ETHEREUM = process.env.ALCHEMY_API_KEY_ETHEREUM
+const ALCHEMY_API_KEY_ETHEREUM = process.env.ALCHEMY_API_KEY
 const ALCHEMY_API_KEY_POLYGON = process.env.ALCHEMY_API_KEY_POLYGON
-
 // Replace this private key with your Ropsten account private key
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
@@ -29,11 +28,11 @@ module.exports = {
   networks: {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY_ETHEREUM}`,
-      accounts: [`${WALLET_PRIVATE_KEY}`]
+      accounts: [`0x${WALLET_PRIVATE_KEY}`]
     },
     mumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY_POLYGON}`,
-      accounts: [`${WALLET_PRIVATE_KEY}`]
+      accounts: [`0x${WALLET_PRIVATE_KEY}`]
     }
   },
   etherscan: {
